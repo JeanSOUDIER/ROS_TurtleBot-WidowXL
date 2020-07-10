9a07d884e31d6cea9aa818d3c45ee8b654ce6ec6
JeanSOUDIER <44265577+JeannotDu67@users.noreply.github.com>
2020-07-10 14:00:36 +0200

    test measure

--- prgm/MATLAB_SCRIPT/Untitled3.m
+++ prgm/MATLAB_SCRIPT/Untitled3.m
@@ -1,22 +1,93 @@
-rosshutdown
-ipTurtlebot = '192.168.1.33';
-rosinit(ipTurtlebot);
+%rosshutdown
+%ipTurtlebot = '192.168.1.33';
+%rosinit(ipTurtlebot);
+%rate = rosrate(100)
 
-subIMU = rossubscriber('/imu');
-subODOM = rossubscriber('/odom');
+Delay = 1;
+Duration = 100; %s
+TAPIS_X = 1500;
+TAPIS_Y = 2200;
+TAPIS_X0 = TAPIS_X/2;
+TAPIS_Y0 = 450;
+Map = zeros(TAPIS_X,TAPIS_Y);
+P = [TAPIS_X0 TAPIS_Y0 0];
+[Map NbPlot] = DiscoverMap(Map, tbot, NbPlot, TAPIS_X, TAPIS_Y, P);
 
-pause(5);
+t = timer;
+t.TimerFcn = @takeBreak;
+t.Period = Delay;
+Imu = [0 0 0 ; 0 0 0];
+Odom = [0 0 0; 0 0 0];
+G = [Imu Odom];
+t.userData = G;
+t.TasksToExecute = round(Duration/Delay);
+t.ExecutionMode = 'fixedRate';
+start(t);
 
-Podom0 = [-1000*subODOM.LatestMessage.Pose.Pose.Position.X -1000*subODOM.LatestMessage.Pose.Pose.Position.Y subODOM.LatestMessage.Pose.Pose.Orientation.Z];
-Pimu0 = [1000*subIMU.LatestMessage.LinearAcceleration.X 1000*subIMU.LatestMessage.LinearAcceleration.Y subIMU.LatestMessage.Orientation.Z];
+%{
+ODOMdata = receive(subODOM,1);
+Podom0 = [-1000*ODOMdata.Pose.Pose.Position.X -1000*ODOMdata.Pose.Pose.Position.Y ODOMdata.Pose.Pose.Orientation.Z];
+IMUdata = receive(subIMU,1);
+Pimu0 = [1000*IMUdata.LinearAcceleration.X 1000*IMUdata.LinearAcceleration.Y IMUdata.LinearAcceleration.Z];
+PimuO0 = [1000*IMUdata.Orientation.X 1000*IMUdata.Orientation.Y IMUdata.Orientation.Z];
+%}
 
-Go([100 -50 0], tbot);
+pause(1);
+%Go([100 0 0], tbot);
+[P Map NbPlot] = PathFinding([TAPIS_X0 1200], Map, tbot, NbPlot, TAPIS_X, TAPIS_Y, P);
 pause(1);
 
-Podom1 = [-1000*subODOM.LatestMessage.Pose.Pose.Position.X -1000*subODOM.LatestMessage.Pose.Pose.Position.Y subODOM.LatestMessage.Pose.Pose.Orientation.Z];
-Pimu1 = [1000*subIMU.LatestMessage.LinearAcceleration.X 1000*subIMU.LatestMessage.LinearAcceleration.Y subIMU.LatestMessage.Orientation.Z];
+stop(t);
 
-Podom = Podom1-Podom0
-Pimu = Pimu1-Pimu0
+G = t.UserData;
+Imu = G(:,1:3);
+ImuAccX = Imu(3:length(Imu),1:1);
+ImuAccY = Imu(3:length(Imu),2:2);
+ImuAccZ = Imu(3:length(Imu),3:3);
+ImuVelX = cumtrapz(ImuAccX);
+ImuVelY = cumtrapz(ImuAccY);
+ImuVelZ = cumtrapz(ImuAccZ);
+ImuPosX = cumtrapz(ImuVelX);
+ImuPosY = cumtrapz(ImuVelY);
+ImuPosZ = cumtrapz(ImuVelZ);
+Odom = G(:,4:6);
+OdomX = Odom(3:length(Odom),1:1);
+OdomY = Odom(3:length(Odom),2:2);
+OdomZ = Odom(3:length(Odom),3:3);
+OdomVelX = [diff(OdomX);0];
+OdomVelY = [diff(OdomY);0];
+OdomVelZ = [diff(OdomZ);0];
 
-
+a = size(ImuAccX)
+t = linspace(1,a(1)+1,a(1));
+size(t)
+figure(1);
+hold on;
+plot(t,ImuAccX,'r-*');
+plot(t,ImuAccY,'b-*');
+plot(t,ImuAccZ,'g-*');
+hold off;
+figure(2);
+hold on;
+plot(t,OdomX,'r-*');
+plot(t,OdomY,'b-*');
+plot(t,OdomZ,'g-*');
+hold off;
+figure(3);
+hold on;
+plot(t,ImuVelX,'r-*');
+plot(t,ImuVelY,'b-*');
+plot(t,ImuVelZ,'g-*');
+hold off;
+figure(4);
+hold on;
+plot(t,ImuPosX,'r-*');
+plot(t,ImuPosY,'b-*');
+plot(t,ImuPosZ,'g-*');
+hold off;
+figure(5);
+hold on;
+plot(t,OdomVelX,'r-*');
+plot(t,OdomVelY,'b-*');
+plot(t,OdomVelZ,'g-*');
+hold off;
--- prgm/MATLAB_SCRIPT/takeBreak.m
+++ prgm/MATLAB_SCRIPT/takeBreak.m
@@ -1,4 +1,13 @@
 function takeBreak(mTimer,~)
-    Coord = mTimer.userData
-    %disp(sub.LatestMessage.Orientation);
+    subIMU = rossubscriber('/imu');
+    subODOM = rossubscriber('/odom');
+    G = mTimer.userData;
+    Imu = G(:,1:3);
+    Odom = G(:,4:6);
+    ODOMdata = receive(subODOM,1);
+    Odom = [Odom ; -1000*ODOMdata.Pose.Pose.Position.X -1000*ODOMdata.Pose.Pose.Position.Y ODOMdata.Pose.Pose.Orientation.Z];
+    IMUdata = receive(subIMU,1);
+    Imu = [Imu ; 1000*IMUdata.Orientation.X 1000*IMUdata.Orientation.Y IMUdata.Orientation.Z];
+    G = [Imu Odom];
+    mTimer.userData = G;
 end
